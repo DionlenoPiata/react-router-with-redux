2900ced66af2d23bc083185fb5e7ab29e88017f1
dionlenoizap <dionleno.oliveira@izap.com.br>
2020-03-12 15:18:50 -0300

    usando parametros na url

--- src/App.js
+++ src/App.js
@@ -4,28 +4,39 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 {
-/*
-- (BrowserRouter) => componente que encapsula os componetes relacionado as rotas
-  - componente inicial
-  - ter que ter um unico elemento filho
-  - ter as rotas
-
-- (Route) => quando um determinado path acontece, ele irá chamar um componente ou renderizar alguma coisa  
-  - chama o componente ou função de rederização
-  - path - caminho que ele espera que seja chamado no navegador
-  - component - componente a ser renderizado
-  - render - função de redenrização
-  - children - sempre vai redenrizar, indiferente da url que vai colocar
-  - exact - renderizar somente esse componente, sem "concatenar com outros..."
-  - strict - so redenriza se o link for exatamente como está na rota.
-*/
-
+  /*
+  - (BrowserRouter) => componente que encapsula os componetes relacionado as rotas
+    - componente inicial
+    - ter que ter um unico elemento filho
+    - ter as rotas
+  
+  - (Route) => quando um determinado path acontece, ele irá chamar um componente ou renderizar alguma coisa  
+    - chama o componente ou função de rederização
+    - path - caminho que ele espera que seja chamado no navegador
+    - component - componente a ser renderizado
+    - render - função de redenrização
+    - children - sempre vai redenrizar, indiferente da url que vai colocar
+    - exact - renderizar somente esse componente, sem "concatenar com outros..."
+    - strict - so redenriza se o link for exatamente como está na rota.
+  */
 }
 
 const Home = () => <div> Home Page</div>
 const About = () => <div> About Page</div>
 const Contact = () => <div> Contact Page</div>
 const Users = ({ match }) => <div> {match.url} </div>
+
+const Params = (props) => {
+
+  let { match, location, history } = props;
+  console.log(match);
+  console.log(location);
+  console.log(history);
+
+  return (
+    <div>Params: { match.params.name } </div>
+  )
+}
 
 function App() {
   return (
@@ -40,17 +51,18 @@ function App() {
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/users">Users</Link></li>
           <li><Link to="/admin">Admin</Link></li>
+          <li><Link to="/params">Params</Link></li>
         </ul>
         <hr />
 
         {/* quando componente ou render responde por uma rotas */}
 
-        <Route exact path="/" component={Home} /> 
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/users" component={Users} />
-        <Route path="/admin" render = {() => <div>Admin area</div> } /> {/* modelo diferente */}
-        <Route children ={() => <div> Vai está aqui para sempre...</div>}/> {/* modelo diferente */}
+        <Route path="/admin" render={() => <div>Admin area</div>} /> {/* modelo diferente */}
+        <Route path="/params/:name" component={Params} />
 
       </div>
     </Router>
