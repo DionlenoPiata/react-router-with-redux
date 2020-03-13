d67fc6e889a0ad1e5d664dbf02a5874146b0c5a0
dionlenoizap <dionleno.oliveira@izap.com.br>
2020-03-13 08:27:33 -0300

    iniciando com actions

--- src/App.js
+++ src/App.js
@@ -3,47 +3,31 @@ import { createStore } from 'redux';
 import './App.css';
 
 class App extends Component {
-
   render() {
 
-    // reducer
-    const reducer = (state = [], action) => {
-      switch (action.type) {
-        case 'ADD_ITEM':
-          return [...state, action.payload] // retorna o que ja tinha mais o item novo. ou seja, muda o estado da aplicação
-        default:
-          return state
+    // action types
+    // usually go to a different file
+    // ADD_ITEM
+    const ADD_ITEM = 'ADD_ITEM'
+
+    // action
+    let action = {
+      type: ADD_ITEM,
+      payload: 'Apple'
+    }
+
+    // action creators
+    // usually go to a different file
+    // item-actions.js
+    const addItem = (item) => {
+      return {
+        type: ADD_ITEM,
+        payload: item
       }
     }
 
-    // action create
-    const addItemToArray = item => {
-      return ({
-        type: 'ADD_ITEM',
-        payload: item
-      })
-    }
-
-
-    // store
-    var store = createStore(reducer)
-
-    console.log("store: ", store.getState());
-    store.dispatch(addItemToArray("ABC"));
-    console.log("store: ", store.getState());
-    store.dispatch(addItemToArray("ABC"));
-    console.log("store: ", store.getState());
-    store.dispatch(addItemToArray("CDF"));
-    console.log("store: ", store.getState());
-    store.dispatch(addItemToArray("GHI"));
-    console.log("store: ", store.getState());
-    store.dispatch(addItemToArray("ABC"));
-    console.log("store: ", store.getState());
-    store.dispatch(addItemToArray("JKL"));
-    console.log("store: ", store.getState());
-
     return (
-      <div>Finish the execution!</div>
+      <div></div>
     );
   }
 }
