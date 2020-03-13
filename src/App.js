f7b6f18d3131afa0a98bb519cf6227e58f64aebc
dionlenoizap <dionleno.oliveira@izap.com.br>
2020-03-13 08:50:17 -0300

    implementando um reducer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ class App extends Component {
     // action types
     // usually go to a different file
     // ADD_ITEM
-    const ADD_ITEM = 'ADD_ITEM'
+    const ADD_ITEM = 'ADD_ITEM';
+    const REMOVE_ITEM = 'REMOVE_ITEM';
 
     // action
-    let action = {
-      type: ADD_ITEM,
-      payload: 'Apple'
-    }
+    //let action = {
+    //  type: ADD_ITEM,
+    //  payload: 'Apple'
+    //  }
 
     // action creators
     // usually go to a different file
@@ -23,6 +24,28 @@ class App extends Component {
       return {
         type: ADD_ITEM,
         payload: item
+      }
+    }
+
+    const removeItem = (item) => {
+      return {
+        type: REMOVE_ITEM,
+        payload: item
+      }
+    }
+
+    // Reducers
+    // ser uma função pura
+    const cartItem = (state = [], action) => {
+
+      switch (action.type) {
+        case ADD_ITEM:
+          return [...state, action.payload]
+
+        case REMOVE_ITEM:
+          return [...state.slice(0, action.payload), ...state.slice(action.payload + 1)]
+        default:
+          return state
       }
     }
 
